72496c76ce4e3c20083683ed18e679720f2fbcd3
BlackShadovV <slava.bsabk@gmail.com>
2017-09-23 15:11:11 +0400

    Fixed name of variables

--- lw1/js/search-not-rnd-array-element.js
+++ lw1/js/search-not-rnd-array-element.js
@@ -1,27 +1,27 @@
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var length = 10,
+var arrLength = 10,
   array = [],
-  key, i;
+  searchKey, i;
 
-for (i = 0; i < length; i++) {
+for (i = 0; i < arrLength; i++) {
   array[i] = getRandomInt(0, 15);
 }
 
 alert("Filled random array " + array.join(' '));
 
 do {
-  key = prompt("Enter number for find ");
-  key = parseInt(key);
-  if ((key == "") || (isNaN(key))) {
+  searchKey = prompt("Enter number for find ");
+  searchKey = parseInt(searchKey);
+  if ((searchKey == "") || (isNaN(searchKey)) || (searchKey == null)) {
     alert("Error!");
   }
-} while ((key == "") || (isNaN(key)))
+} while ((searchKey == "") || (isNaN(searchKey)) || (searchKey == null));
 
-if (array.indexOf(key) !== -1) {
-  alert("Element " + key + " is found!");
+if (array.indexOf(searchKey) !== -1) {
+  alert("Element " + searchKey + " is found!");
 } else {
-  alert("Element " + key + " not found!");
+  alert("Element " + searchKey + " not found!");
 }
