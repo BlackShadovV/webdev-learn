cdc23e74eb16aee24a1b59b2f1529a1fa135d807
BlackShadovV <slava.bsabk@gmail.com>
2017-10-21 15:03:05 +0400

    rework lab on jquery

--- lw3/js/verification.js
+++ lw3/js/verification.js
@@ -1,12 +1,11 @@
-/*Написать программу определения корректности ввода пользователем адреса электронной почты. Программа должна проверять следующие условия:
-1. наличие символа «@»;
-2. минимальный размер адреса – 6 символов (например, a@b.by);
-3. слева от символа «@» должен быть, как минимум, один символ;
-4. справа от символа «@» должна быть, как минимум, одна точка;
-5. справа от последней точки должно быть, как минимум, 2 символа;
-6. между символом «@» и следующей за ним точкой должен быть, как минимум, один символ.
-*/
-
+function mailCheck(mail) {
+  var mask = mail.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  if (!mask) {
+    alert('Введите валидный E-mail');
+    return false;
+  }
+  return true;
+}
 
 function passCheck(password, passwordConfirm) {
   if (password.length < 6) {
@@ -21,7 +20,7 @@ function passCheck(password, passwordConfirm) {
 }
 
 function checkRules() {
-  var check = document.getElementsByClassName("check")[0].checked;
+  var check = $(".check").prop("checked");
   if (!check) {
     alert("вы не приняли соглашение");
     return false;
@@ -29,20 +28,11 @@ function checkRules() {
   return true;
 }
 
-function mailCheck(mail) {
-  var mask = mail.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-  if (!mask) {
-    alert('Введите валидный E-mail');
-    return false;
-  }
-  return true;
-}
-
-function main() {
-  var email = document.getElementsByClassName("login")[0].value;
-  var pass = document.getElementsByClassName("password")[0].value;
-  var passConfirm = document.getElementsByClassName("confirmPassword")[0].value;
-  if (mailCheck(email) && passCheck(pass, passConfirm) && checkRules()) {
+$(".registration").submit(function() {
+  var email = $(".login");
+  var pass = $(".password");
+  var passConfirm = $(".confirmPassword");
+  if (mailCheck($(email[0]).val()) && passCheck($(pass[0]).val(), $(passConfirm[0]).val()) && checkRules()) {
     alert("регистрация прошла успешно");
   }
-}
+});
