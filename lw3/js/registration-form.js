4be25dc5ad9ac8c9c0a6503b5f12e8abcc57ab81
BlackShadovV <slava.bsabk@gmail.com>
2017-11-01 18:46:31 +0400

    rework remark on jquery branch

--- lw3/js/registration-form.js
+++ lw3/js/registration-form.js
@@ -1,70 +1,29 @@
-var errorNumber = 0
-
-function mailCheck(mail) {
-  var mask = mail.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-  if (!mask) {
-    errorNumber = 1;
-    return false;
-  }
-  return true;
-}
-
-function passCheck(password, passwordConfirm) {
-  if (password.length < 6) {
-    errorNumber = 2;
-    return false;
-  }
-  if (password != passwordConfirm) {
-    errorNumber = 3;
-    return false;
-  }
-  return true;
-}
-/*
-function checkRules() {
+function validateForm(password, passwordConfirm, mail) {
   var check = $(".check").prop("checked");
-  if (($(".check").prop("checked"))) {
-    errorNumber = 4;
-    return false;
-  }
-  return true;
-}
-*/
-function checkRules() {
-  if ($(".check").prop("checked")) {
+  var mask = mail.val().match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  if (!mask) {
+    return 'Некорректный E-mail';
+  } else if (password.val().length < 6) {
+    return 'Пароль короткий';
+  } else if (password.val() != passwordConfirm.val()) {
+    return 'Пароли не совпадают';
+  } else if (!check) {
+    return 'Примите соглашение';
+  } else {
     return true;
   }
-  errorNumber = 4;
-  return false;
 }
 
-
-$(".registration").submit(function() {
-  var email = $(".login");
+$(".registration").submit(function(event) {
+  var mail = $(".login");
   var pass = $(".password");
   var passConfirm = $(".confirmPassword");
-  if (mailCheck($(email[0]).val()) && passCheck($(pass[0]).val(), $(passConfirm[0]).val()) && checkRules()) {
-    alert("регистрация прошла успешно");
+  var validateResult = validateForm(pass, passConfirm, mail)
+  if (validateResult === true) {
+    alert("Регистрация прошла успешно!");
   } else {
-    switch (errorNumber) {
-      case 1:
-        errorMessage = 'Некорректный E-mail';
-        break;
-      case 2:
-        errorMessage = 'Короткий пароль';
-        break;
-      case 3:
-        errorMessage = 'Пароли не совпадают';
-        break;
-      case 4:
-        errorMessage = 'Примите соглашение';
-        break;
-      default:
-        errorMessage = 'Что-то пошло не так';
-        break;
-    }
-    alert(errorMessage);
-    return false;
+    alert(validateResult);
+    event.preventDefault();
   }
 });
 
